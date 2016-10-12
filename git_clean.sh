a73c094949269776562eadcd6e0c22db905ec3f3
calvinwilliams <calvinwilliams.c@gmail.com>
2016-10-12 14:59:34 +0800

    UPDATE TO V0.12.1E BETA

--- git_clean.sh
+++ git_clean.sh
@@ -1,6 +1,6 @@
 git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch setup.exe' --prune-empty --tag-name-filter cat HEAD -- --all
 git push origin master --force  
-rm -Force -R .git/refs/original/  
+rm -rf .git/refs/original/  
 git reflog expire --expire=now --all  
 git gc --prune=now  
 git gc --aggressive --prune=now
